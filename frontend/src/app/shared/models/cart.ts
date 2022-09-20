ada4d3b70ee00d718267717b09971b062e40b0b6
Mohamed Gamal <90586098+MohamedJimmy20@users.noreply.github.com>
2022-09-20 23:53:49 +0200

    Delete frontend directory

--- frontend/src/app/shared/models/cart.ts
+++
@@ -1,7 +0,0 @@
-import { cartItem } from './../cartitem';
-export class Cart{
-  items:cartItem[]= [];
-  totalPrice: number = 0;
-  totalCount: number = 0;
-
-}
--- frontend/src/app/shared/models/user.ts
+++
@@ -1,9 +0,0 @@
-export class user{
-  id!: string;
-  email!: string;
-  name!: string;
-  address!: string;
-  token!: string;
-  isAdmin!: boolean;
-
-}
